6389632d70e1f007bface0a2dc91b0e2e7125b5b
Mauricio Farinelli <maufarinelli@gmail.com>
2015-06-05 17:45:51 -0400

    Start using ES6 with babel and Browserify

---
+++ modules/palmeiras.js
@@ -0,0 +1,11 @@
+/**
+ * Class Palmeiras
+ */
+class Palmeiras {
+    constructor(options) {
+        this.settings = options.settings;
+        this.elements = options.elements;
+    }
+}
+
+export { Palmeiras };
